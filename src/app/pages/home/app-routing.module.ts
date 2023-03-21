0b6381088c317855c1bd3df3cd71630fd7fe01d2
Rafa31263 <rafael.perez@w2m.com>
2023-03-21 12:21:02 +0100

    first angular project

---
+++ src/app/pages/home/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+
+const routes: Routes = [
+
+  {
+    path: 'test',
+    component:HeaderComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
